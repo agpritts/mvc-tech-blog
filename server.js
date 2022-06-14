a4f0f8b1fc444d485422f468263da00eccaaf5d4
erma <agpritts@gmail.com>
2022-06-14 14:40:50 -0400

    middleware errors :(

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3382;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const exphbs = require('express-handlebars');
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App launched on ${PORT}`));
-  });
+  app.listen(PORT, () => console.log(`Active on ${PORT}`));
+});
