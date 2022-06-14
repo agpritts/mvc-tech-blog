740ef525db3f7ba51e5904b8afb30e37ad870a36
erma <agpritts@gmail.com>
2022-06-14 14:56:15 -0400

    server errors resolved

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User } = require('../models/User.js');
 
 router.get('/', (req, res) => {
-    console.log("home get req successful")
+    console.log("home get req successful");
+    res.render('layouts/main');
 }),
 
 module.exports = router;
