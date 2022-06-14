dc85e22f3c1016a576080dc1e0aa903c1c24c2eb
erma <agpritts@gmail.com>
2022-06-14 13:37:06 -0400

    server init/config

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-// server
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
